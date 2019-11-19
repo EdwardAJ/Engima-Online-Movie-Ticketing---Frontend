784872be43f6e553081ff6ef3ce086656748243c
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2019-11-19 23:56:44 +0700

    Ganti lokasi

--- deploy.sh
+++ deploy.sh
@@ -34,5 +34,5 @@ echo "ALL_SERVERS ${ALL_SERVERS}"
 for server in "${ALL_SERVERS[@]}"
 do
   echo "deploying to ${server}"
-  ssh ubuntu@${server} 'bash -s' < ./deploy/updateAndRestart.sh
+  ssh ubuntu@${server} 'bash -s' < ./updateAndRestart.sh
 done
