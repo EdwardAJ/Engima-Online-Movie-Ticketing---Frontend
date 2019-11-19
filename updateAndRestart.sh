58eb4e26be10e37f7eeee10786aa90fa00820b20
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2019-11-19 23:56:05 +0700

    Ganti folder

--- updateAndRestart.sh
+++ updateAndRestart.sh
@@ -12,24 +12,24 @@ git clone https://gitlab.informatika.org/if3110-2019-02-k01-06/engima.git
 #source the nvm file. In an non
 #If you are not using nvm, add the actual path like
 # PATH=/home/ubuntu/node/bin:$PATH
-source /home/ubuntu/.nvm/nvm.sh
+#source /home/ubuntu/.nvm/nvm.sh
 
 # stop the previous pm2
-pm2 kill
-npm remove pm2 -g
+# pm2 kill
+# npm remove pm2 -g
 
 
 #pm2 needs to be installed globally as we would be deleting the repo folder.
 # this needs to be done only once as a setup script.
-npm install pm2 -g
-# starting pm2 daemon
-pm2 status
+# npm install pm2 -g
+# # starting pm2 daemon
+# pm2 status
 
-cd /home/ubuntu/ci_cd_demo
-
+#cd /home/ubuntu/ci_cd_demo
+cd engima
 #install npm packages
 echo "Running npm install"
-npm install
+# npm install
 
-#Restart the node server
-npm start
+# #Restart the node server
+# npm start
