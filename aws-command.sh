f30c8f77e43644a1e73464128bd4217312d52f49
Aidil Rezjki S S <13517070@std.stei.itb.ac.id>
2019-11-20 20:29:44 +0700

    Test again CD.

--- aws-command.sh
+++ aws-command.sh
@@ -9,6 +9,8 @@ git stash
 git pull origin deployCICD
 echo 'Removing existing container...'
 sudo docker-compose stop engima_php && sleep 10
+echo 'Creating .env'
+cp ENV.SAMPLE .env
 echo 'Run the container...'
 sudo docker-compose up -d
 echo 'Process done.'
