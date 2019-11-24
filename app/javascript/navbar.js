441255c811edbc947f1f048e1812248522dc72ec
Tanor Abraham Reyuko <13516088@std.stei.itb.ac.id>
2019-11-24 14:36:00 +0700

    Repaired Google Sign in (Added logout)

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,7 +1,15 @@
 function logoutAction() {
+    signOut();
     deleteCookie('LOGIN_HASH');
     deleteCookie('USERNAME');
     location.reload();
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
 }
 
 function searchAction() {
